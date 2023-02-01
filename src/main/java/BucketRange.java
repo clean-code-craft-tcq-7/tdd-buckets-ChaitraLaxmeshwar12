3a4ac24a088e8dda0b0ef3f7c9dc403706dbefb4
Chaitra Laxmeshwar <25486620+ChaitraLaxmeshwar12@users.noreply.github.com>
2023-02-01 16:33:56 +0530

    Update BucketRange.java

--- src/main/java/BucketRange.java
+++ src/main/java/BucketRange.java
@@ -2,32 +2,10 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 public class BucketRange
 {
-  
-  public static void printRangeandReadings(int[] readings) {
-		Map<String, Integer> rangeandCount = getRangeandReadings(readings);
-		if (rangeandCount.size() > 0) {
-			System.out.println("Range, Readings");
-			for (Map.Entry<String, Integer> entry : rangeandCount.entrySet()) {
-				System.out.println(entry.getKey() + ", " + entry.getValue());
-
-			}
-		}
-	}
-
-	  public static String getRange(int firstPoint, int lastPoint) {
-		String range = null;
-		if (firstPoint == lastPoint)
-			range = String.valueOf(firstPoint);
-		else
-			range = String.valueOf(firstPoint) + "-" + String.valueOf(lastPoint);
-		return range;
-	}
-
-	public static Map<String, Integer> getRangeandReadings(int[] readings) {
+	 public static Map<String, Integer> getRangeandReadings(int[] readings) {
 		Map<String, Integer> rangeandCount = new LinkedHashMap<String, Integer>();
 		if (readings.length > 1) {
-			int count = 1;
-			int minInRange = readings[0], maxInRange = readings[0];
+			int count = 1, minInRange = readings[0], maxInRange = readings[0];
 			for (int i = 1; i < readings.length; i++) {
 				if ((readings[i] == readings[i - 1]) || (readings[i] == readings[i - 1] + 1)) {
 					count++;
@@ -36,11 +14,17 @@ public class BucketRange
 					rangeandCount.put(getRange(minInRange, maxInRange), count);
 					count = 1;
 					minInRange = readings[i];
-				}
-			}
+				} }
 			maxInRange = readings[readings.length - 1];
 			rangeandCount.put(getRange(minInRange, maxInRange), count);
 		}
 		return rangeandCount;
+	}}
+          public static String getRange(int firstPoint, int lastPoint) {
+		String range = null;
+		if (firstPoint == lastPoint)
+			range = String.valueOf(firstPoint);
+		else
+			range = String.valueOf(firstPoint) + "-" + String.valueOf(lastPoint);
+		return range;
 	}
-}
