19adf74522e893b2b80352cf5e41031afa72fb7e
Chaitra Laxmeshwar <25486620+ChaitraLaxmeshwar12@users.noreply.github.com>
2023-02-01 22:20:23 +0530

    Update BucketRangeTest.java

--- src/main/java/BucketRangeTest.java
+++ src/main/java/BucketRangeTest.java
@@ -1,5 +1,35 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 public class BucketRangeTest
 {
-  
+  @Test
+	public void testRangeCreater() {
+		Map<String,Integer> rangeandcount;
+		Map.Entry<String, Integer> Entry;
+		//sample test
+		rangeandcount= App.getRangeandReadings(new int[]{4,5});
+		Map.Entry<String, Integer> firstEntry = rangeandcount.entrySet().iterator().next();
+		assertEquals("4-5",firstEntry.getKey());
+		assertEquals(new Integer(2),firstEntry.getValue());
+		
+		// same numbers
+		rangeandcount = App.getRangeandReadings(new int[]{5,5,5,5});
+		Entry = rangeandcount.entrySet().iterator().next();
+		assertEquals("5",Entry.getKey());
+		assertEquals(new Integer(4),Entry.getValue());
+		
+		// unsorted array
+		rangeandcount = App.getRangeandReadings(new int[]{2,6,4,5,3});
+		Entry = rangeandcount.entrySet().iterator().next();
+		assertEquals("2-6",Entry.getKey());
+		assertEquals(new Integer(5),Entry.getValue());
+		
+	}
 
 }
