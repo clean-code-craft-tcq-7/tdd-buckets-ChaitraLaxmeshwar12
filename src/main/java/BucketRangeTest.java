100d4f0c6b689ffc6cec1f75a54584705051536a
Chaitra Laxmeshwar <25486620+ChaitraLaxmeshwar12@users.noreply.github.com>
2023-02-02 11:17:33 +0530

    Update BucketRangeTest.java

--- src/main/java/BucketRangeTest.java
+++ src/main/java/BucketRangeTest.java
@@ -15,19 +15,19 @@ public class BucketRangeTest
 		Map<String,Integer> rangeandcount;
 		Map.Entry<String, Integer> Entry;
 		//sample test
-		rangeandcount= App.getRangeandReadings(new int[]{4,5});
+		rangeandcount= BucketRange.getRangeandReadings(new int[]{4,5});
 		Map.Entry<String, Integer> firstEntry = rangeandcount.entrySet().iterator().next();
 		assertEquals("4-5",firstEntry.getKey());
 		assertEquals(new Integer(2),firstEntry.getValue());
 		
 		// same numbers
-		rangeandcount = App.getRangeandReadings(new int[]{5,5,5,5});
+		rangeandcount = BucketRange.getRangeandReadings(new int[]{5,5,5,5});
 		Entry = rangeandcount.entrySet().iterator().next();
 		assertEquals("5",Entry.getKey());
 		assertEquals(new Integer(4),Entry.getValue());
 		
 		// unsorted array
-		rangeandcount = App.getRangeandReadings(new int[]{2,6,4,5,3});
+		rangeandcount = BucketRange.getRangeandReadings(new int[]{2,6,4,5,3});
 		Entry = rangeandcount.entrySet().iterator().next();
 		assertEquals("2-6",Entry.getKey());
 		assertEquals(new Integer(5),Entry.getValue());
